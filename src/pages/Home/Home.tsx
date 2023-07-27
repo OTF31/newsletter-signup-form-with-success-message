0c62b44f5dbbe6f1108e020680db26810f75276b
Denis Omar Cuyo Ttito <182906@unsaac.edu.pe>
2023-07-27 09:21:26 -0500

    add success mobile modal

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,15 @@
-import { AppShell, Group, Image, Space, Stack, Text } from '@mantine/core';
+import { AppShell, Box, Group, Image, Modal, Space, Stack, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
-import iconList from '@/assets/images/icon-list.svg';
+import { ReactComponent as IconList } from '@/assets/images/icon-list.svg';
+import { ReactComponent as IconSuccess } from '@/assets/images/icon-success.svg';
 import ilustrationMobile from '@/assets/images/illustration-sign-up-mobile.svg';
 import Button from '@/components/Button';
 import TextInput from '@/components/TextInput';
 
 const Home = () => {
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
     <AppShell padding={0}>
       <Stack spacing={40} mb={40}>
@@ -18,15 +22,15 @@ const Home = () => {
             <Text>Join 60,000+ product managers receiving monthly updates on:</Text>
             <Stack spacing={10}>
               <Group noWrap align='flex-start'>
-                <Image src={iconList} alt='icon-list' width={21} />
+                <IconList />
                 <Text>Product discovery and building what matters</Text>
               </Group>
               <Group noWrap align='flex-start'>
-                <Image src={iconList} alt='icon-list' width={21} />
+                <IconList />
                 <Text>Measuring to ensure updates are a success</Text>
               </Group>
               <Group noWrap align='flex-start'>
-                <Image src={iconList} alt='icon-list' width={21} />
+                <IconList />
                 <Text>And much more!</Text>
               </Group>
             </Stack>
@@ -34,10 +38,35 @@ const Home = () => {
           <Stack spacing={0}>
             <TextInput label='Email address' placeholder='email@company.com' />
             <Space h={16} />
-            <Button>Subscribe to monthly newsletter</Button>
+            <Button onClick={open}>Subscribe to monthly newsletter</Button>
           </Stack>
         </Stack>
       </Stack>
+      <Modal
+        opened={opened}
+        onClose={close}
+        withCloseButton={false}
+        fullScreen
+        transitionProps={{ transition: 'pop', duration: 400 }}
+      >
+        <Box mt={135} c='darkNavy'>
+          <IconSuccess />
+          <Text size={40} weight={700} mt={40} mb='xl' lh='100%'>
+            Thanks for subscribing!
+          </Text>
+          <Text>
+            A confirmation email has been sent to
+            <Text component='span' weight={700}>
+              {' '}
+              ash@loremcompany.com
+            </Text>
+            . Please open it and click the button inside to confirm your subscription
+          </Text>
+        </Box>
+        <Button fullWidth mt={250} onClick={close}>
+          Dismiss message
+        </Button>
+      </Modal>
     </AppShell>
   );
 };
