eadd4945eee6cc587e02d333cdd6a5e36d1f9639
Denis Omar Cuyo Ttito <182906@unsaac.edu.pe>
2023-07-27 19:47:38 -0500

    add desktop design
    - Update modal design for desktop screens

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,57 +1,102 @@
-import { AppShell, Box, Group, Image, Modal, Space, Stack, Text } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
+import {
+  AppShell,
+  Box,
+  Container,
+  Flex,
+  Group,
+  Image,
+  Modal,
+  Space,
+  Stack,
+  Text,
+} from '@mantine/core';
+import { useDisclosure, useMediaQuery } from '@mantine/hooks';
 
 import { ReactComponent as IconList } from '@/assets/images/icon-list.svg';
 import { ReactComponent as IconSuccess } from '@/assets/images/icon-success.svg';
 import ilustrationMobile from '@/assets/images/illustration-sign-up-mobile.svg';
+import ilustrationDesktop from '@/assets/images/illustration-sign-up-desktop.svg';
 import Button from '@/components/Button';
 import TextInput from '@/components/TextInput';
 
 const Home = () => {
   const [opened, { open, close }] = useDisclosure(false);
+  const desktop = useMediaQuery('(min-width: 1440px)');
 
   return (
-    <AppShell padding={0}>
-      <Stack spacing={40} mb={40}>
-        <Image src={ilustrationMobile} alt='ilustration' />
-        <Stack spacing={40} px='xl'>
-          <Stack c='darkNavy' spacing='xl'>
-            <Text size={40} weight={700}>
-              Stay updated!
-            </Text>
-            <Text>Join 60,000+ product managers receiving monthly updates on:</Text>
-            <Stack spacing={10}>
-              <Group noWrap align='flex-start'>
-                <IconList />
-                <Text>Product discovery and building what matters</Text>
-              </Group>
-              <Group noWrap align='flex-start'>
-                <IconList />
-                <Text>Measuring to ensure updates are a success</Text>
-              </Group>
-              <Group noWrap align='flex-start'>
-                <IconList />
-                <Text>And much more!</Text>
-              </Group>
+    <AppShell
+      padding={0}
+      styles={theme => ({
+        main: {
+          [theme.fn.largerThan('xl')]: { backgroundColor: theme.colors.darkNavy[6] },
+        },
+      })}
+    >
+      <Flex mih='100vh' align={{ xl: 'center' }}>
+        <Container
+          fluid={!desktop}
+          px={0}
+          p={{ xl: 'xl' }}
+          sx={theme => ({
+            [theme.fn.largerThan('xl')]: { borderRadius: 36 },
+          })}
+          bg='white'
+          w={{ base: '100%' }}
+        >
+          <Flex
+            direction={{ base: 'column', xl: 'row-reverse' }}
+            gap={40}
+            pb={{ base: 100, xl: 0 }}
+          >
+            <Image
+              src={desktop ? ilustrationDesktop : ilustrationMobile}
+              fit='cover'
+              alt='ilustration'
+              maw={{ xl: 400 }}
+            />
+            <Stack spacing={40} px='xl' mt={{ xl: 40 }}>
+              <Stack c='darkNavy' spacing='xl'>
+                <Text fz={{ base: 40, xl: 56 }} weight={700}>
+                  Stay updated!
+                </Text>
+                <Text>Join 60,000+ product managers receiving monthly updates on:</Text>
+                <Stack spacing={10}>
+                  <Group noWrap align='flex-start'>
+                    <IconList />
+                    <Text>Product discovery and building what matters</Text>
+                  </Group>
+                  <Group noWrap align='flex-start'>
+                    <IconList />
+                    <Text>Measuring to ensure updates are a success</Text>
+                  </Group>
+                  <Group noWrap align='flex-start'>
+                    <IconList />
+                    <Text>And much more!</Text>
+                  </Group>
+                </Stack>
+              </Stack>
+              <Stack spacing={0}>
+                <TextInput label='Email address' placeholder='email@company.com' />
+                <Space h={16} />
+                <Button onClick={open}>Subscribe to monthly newsletter</Button>
+              </Stack>
             </Stack>
-          </Stack>
-          <Stack spacing={0}>
-            <TextInput label='Email address' placeholder='email@company.com' />
-            <Space h={16} />
-            <Button onClick={open}>Subscribe to monthly newsletter</Button>
-          </Stack>
-        </Stack>
-      </Stack>
+          </Flex>
+        </Container>
+      </Flex>
       <Modal
         opened={opened}
         onClose={close}
         withCloseButton={false}
-        fullScreen
+        fullScreen={!desktop}
+        centered
         transitionProps={{ transition: 'pop', duration: 400 }}
+        radius={36}
+        size={500}
       >
-        <Box mt={135} c='darkNavy'>
+        <Box mt={{ base: 135, xl: 30 }} c='darkNavy' px={{ xl: 48 }} pb={{ xl: 64 }}>
           <IconSuccess />
-          <Text size={40} weight={700} mt={40} mb='xl' lh='100%'>
+          <Text fz={{ base: 40, xl: 56 }} weight={700} mt={40} mb='xl' lh='100%'>
             Thanks for subscribing!
           </Text>
           <Text>
@@ -61,11 +106,11 @@ const Home = () => {
               ash@loremcompany.com
             </Text>
             . Please open it and click the button inside to confirm your subscription
+            <Button fullWidth mt={{ base: 250, xl: 40 }} onClick={close}>
+              Dismiss message
+            </Button>
           </Text>
         </Box>
-        <Button fullWidth mt={250} onClick={close}>
-          Dismiss message
-        </Button>
       </Modal>
     </AppShell>
   );
